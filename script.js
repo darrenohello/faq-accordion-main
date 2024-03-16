31bb099ea1d49fb69f45557b269a4e6e49735fd8
Darren <darren.ohehir@ausmed.com.au>
2024-03-16 22:01:25 +1100

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $(".accordion").on("click", ".heading", function() {
+
+   $(this).toggleClass("active").next().slideToggle();
+
+   $(".contents").not($(this).next()).slideUp(300);
+                
+   $(this).siblings().removeClass("active");
+   });
+  });
